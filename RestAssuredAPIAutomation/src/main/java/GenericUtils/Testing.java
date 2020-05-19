3d65514e3b924fa60a48a2841529fd1ee368ea33
NilavaPal <palnilava03@gmail.com>
2020-05-19 22:46:41 +0530

    Nilava Commit the code

---
+++ RestAssuredAPIAutomation/src/main/java/GenericUtils/Testing.java
@@ -0,0 +1,15 @@
+package GenericUtils;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import io.restassured.RestAssured;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello World");
+	}
+
+}
